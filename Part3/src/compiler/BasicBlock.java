fcc7d09033d94475b1a42e7c70751b281dca78bd
MaxouSenpai <hauwaertmaxime@gmail.com>
2020-12-18 21:06:51 +0100

    BasicBlock Javadoc

--- Part3/src/compiler/BasicBlock.java
+++ Part3/src/compiler/BasicBlock.java
@@ -3,19 +3,37 @@ package compiler;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents a basic block of code.
+ */
 public class BasicBlock {
     String label;
     List<String> lines;
 
+    /**
+     * Construct the basic block with the specified label.
+     *
+     * @param label The label of the basic block
+     */
     public BasicBlock(String label) {
         this.label = label;
         this.lines = new ArrayList<>();
     }
 
+    /**
+     * Add a line to the basic block.
+     *
+     * @param line The line to add
+     */
     public void add(String line) {
         this.lines.add(line);
     }
 
+    /**
+     * Return the code contained in the basic block.
+     *
+     * @return The code contained in the basic block
+     */
     public String toString() {
         StringBuilder code = new StringBuilder();
         code.append("\t").append(this.label).append(":\n");
@@ -26,5 +44,4 @@ public class BasicBlock {
 
         return code.toString();
     }
-
 }
